15367c80c51abafbe16363468736881e55794cbd
David W. Arnold <d.w.arnold.gm@gmail.com>
2020-09-28 18:03:36 +0100

    Task 2 attempt

--- app/Models/Question.php
+++ app/Models/Question.php
@@ -6,5 +6,29 @@ use Illuminate\Database\Eloquent\Model;
 
 class Question extends Model
 {
-    //
+    protected $questionnaireBelongsTo;
+
+    /**
+     * Question constructor.
+     */
+    public function __construct()
+    {
+        $this->questionnaireBelongsTo = Question::find(1)->hasQuestionnaire;
+    }
+
+    /**
+     * Get the questionnaire record associated with the question.
+     */
+    public function hasQuestionnaire()
+    {
+        return $this->hasOne('App\Models\Questionnaire');
+    }
+
+    /**
+     * Get the questionnaire that owns the question.
+     */
+    public function belongsToQuestionnaire()
+    {
+        return $this->belongsTo('App\Models\Questionnaire');
+    }
 }
--- app/Models/Questionnaire.php
+++ app/Models/Questionnaire.php
@@ -6,5 +6,29 @@ use Illuminate\Database\Eloquent\Model;
 
 class Questionnaire extends Model
 {
-    protected $fillable = ['name'];
+    protected $questions;
+
+    /**
+     * Questionnaire constructor.
+     */
+    public function __construct()
+    {
+        $this->questions = Questionnaire::find(1)->hasQuestions;
+    }
+
+    /**
+     * Get the question records associated with the questionnaire.
+     */
+    public function hasQuestions()
+    {
+        return $this->hasMany('App\Models\Question');
+    }
+
+    /**
+     * Get the questions that belong to the questionnaire.
+     */
+    public function belongsToQuestions()
+    {
+        return $this->belongsToMany('App\Models\Question');
+    }
 }
